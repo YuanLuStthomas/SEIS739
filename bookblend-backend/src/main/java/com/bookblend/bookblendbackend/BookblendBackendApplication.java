9544434892847613be9f75f2558924e9145eba0d
apafko <ashley.pafko@optum.com>
2024-04-13 20:23:19 -0500

    Base code for project

---
+++ bookblend-backend/src/main/java/com/bookblend/bookblendbackend/BookblendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookblend.bookblendbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookblendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookblendBackendApplication.class, args);
+	}
+
+}
---
+++ bookblend-backend/src/test/java/com/bookblend/bookblendbackend/BookblendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookblend.bookblendbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookblendBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
