6aeff3761b3b5876a8c51ffa4f8434182ebf41a2
apafko <ashley.pafko@optum.com>
2024-04-14 20:11:33 -0500

    Base code for project

---
+++ bookblend-backend/src/main/java/com/bookblend/bookblendbackend/TestController.java
@@ -0,0 +1,13 @@
+package com.bookblend.bookblendbackend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "TEST";
+    } 
+
+}
